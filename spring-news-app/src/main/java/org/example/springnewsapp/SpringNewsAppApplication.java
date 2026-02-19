1b9608a99d562603b242a78286ba935607b1cc77
Viji-qmofi <vijiramu@gmail.com>
2026-02-19 08:46:16 -0600

    Adding my first backend app

---
+++ spring-news-app/src/main/java/org/example/springnewsapp/SpringNewsAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.springnewsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNewsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNewsAppApplication.class, args);
+    }
+
+}
---
+++ spring-news-app/src/test/java/org/example/springnewsapp/SpringNewsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springnewsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNewsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
