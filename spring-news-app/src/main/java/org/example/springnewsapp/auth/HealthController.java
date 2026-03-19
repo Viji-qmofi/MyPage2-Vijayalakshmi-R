c2ae639d510ed3b32e797f8809572e21a033cfc7
Viji-qmofi <vijiramu@gmail.com>
2026-03-19 12:36:44 -0500

    update all for aws

--- spring-news-app/src/main/java/org/example/springnewsapp/auth/HealthController.java
+++ spring-news-app/src/main/java/org/example/springnewsapp/auth/HealthController.java
@@ -1,4 +1,14 @@
 package org.example.springnewsapp.auth;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
+
+    @GetMapping("/api/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
 }
--- spring-news-app/src/main/java/org/example/springnewsapp/security/config/AppBeansConfig.java
+++ spring-news-app/src/main/java/org/example/springnewsapp/security/config/AppBeansConfig.java
@@ -4,12 +4,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class AppBeansConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(
+                "https://mypagefullstack.netlify.app"
+        ));
+
+        configuration.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
